e2c56e6c7a963a7fd0022c5cf7ba0ea69d55dfe9
redmblue <47989545+redmblue@users.noreply.github.com>
2025-05-07 21:34:58 -0400

    Add measurement id for future firebase analytics support

--- app.config.js
+++ app.config.js
@@ -64,7 +64,8 @@ export default () => ({
         projectId: process.env.FIREBASE_PROJECT_ID,
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
       }
     },
     owner: "pantry-ai"
